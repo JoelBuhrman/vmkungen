53fbbea27c374aedec934381169fdf0fdce49174
Joel Buhrman <joel.buhrman@live.se>
2018-06-08 12:33:20 +0200

    Try if db messing up heroku

--- index.js
+++ index.js
@@ -6,47 +6,55 @@ const app = express();
 var mysql = require('mysql')
 var db = require('./db')
 var token = require('./tokens')
-db.connect()
+//db.connect()
 
 
 
 app.get('/api/getHash/:username', (req, res) =>{
-    db.getHash(req.params.username, result => result ? res.send(result) : res.send([false]))
+    res.send([true]) 
+    //db.getHash(req.params.username, result => result ? res.send(result) : res.send([false]))
 });
 
 app.get('/api/signup/:username/:hashedpassword', (req, res) =>{
     const t = token.generateToken()
-    db.createUser(req.params.username, req.params.hashedpassword, t, result => result === true ? res.send([t]) : res.send([result]))
+    res.send([true])
+    //db.createUser(req.params.username, req.params.hashedpassword, t, result => result === true ? res.send([t]) : res.send([result]))
 });
 
 
 app.get('/api/checkToken/:token', (req, res) =>{
-    db.getUserWithToken(req.params.token, result => result ? res.send(result) : res.send([false]))
+    res.send([true])
+    //db.getUserWithToken(req.params.token, result => result ? res.send(result) : res.send([false]))
 });
 
 
 app.get('/api/generateToken/:username', (req, res) =>{
 	const t = token.generateToken()
-    db.generateToken(req.params.username, t, result => result ? res.send([t]) : res.send([false]))
+    res.send([true])
+    //db.generateToken(req.params.username, t, result => result ? res.send([t]) : res.send([false]))
 });
 
 app.get('/api/getGames/:username', (req, res) =>{
-    db.getGames(req.params.username, result => result ? res.send(result) : res.send([false]))
+    res.send([true])
+    //db.getGames(req.params.username, result => result ? res.send(result) : res.send([false]))
 });
 
 
 app.get('/api/updateGuesses/:username/:id/:homeTeam/:awayTeam', (req, res) =>{
-    db.updateGuesses(req.params.username, req.params.id, req.params.homeTeam, req.params.awayTeam, result => result ? res.send([result]) : res.send([false]))
+    res.send([true])
+    //db.updateGuesses(req.params.username, req.params.id, req.params.homeTeam, req.params.awayTeam, result => result ? res.send([result]) : res.send([false]))
 });
 
 
 app.get('/api/getUsers', (req, res) =>{
-    db.getUsers(result => result ? res.send([result]) : res.send([false]))
+    res.send([true])
+    //db.getUsers(result => result ? res.send([result]) : res.send([false]))
 });
 
 
 app.get('/api/updateResults', (req, res) =>{
-    db.updateResults(result => result ? res.send([result]) : res.send([false]))
+    res.send([true])
+    //db.updateResults(result => result ? res.send([result]) : res.send([false]))
 });
 
 
